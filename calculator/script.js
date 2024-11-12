3f5d0d53abe8c2d8cfa6ba45121cbf5e844fe0d4
Vajdani <50171679+Vajdani@users.noreply.github.com>
2024-11-12 13:46:06 +0100

    1. feladat

---
+++ calculator/script.js
@@ -0,0 +1,9 @@
+var isOn = false
+function Toggle() {
+    isOn = !isOn
+    document.getElementById("calc").style.display = isOn ? "flex" : "none"
+}
+
+function maximize() {
+    window.alert("ball")
+}
