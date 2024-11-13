e0ab5166c3eb6fd194f30cb5b93e03d4a7350ef7
Vajdani <50171679+Vajdani@users.noreply.github.com>
2024-11-13 09:38:41 +0100

    2. feladat

--- calculator/script.js
+++ calculator/script.js
@@ -1,8 +1,52 @@
 var isOn = false
 var lastAction
-var num = 0
-var numCache = 0
-var numStr = ""
+var num = "0"
+var numCache = "0"
+
+var numLengthLimit = 12
+
+var opToChar = {
+    "div" : " / ",
+    "mul" : " * ",
+    "sub" : " - ",
+    "add" : " + ",
+}
+
+var codeToAction = {
+    "Backspace"      : "ce",
+    "Delete"         : "c",
+    "NumpadDivide"   : "div",
+    "NumpadMultiply" : "mul",
+    "NumpadSubtract" : "sub",
+    "NumpadAdd"      : "add",
+    "NumpadEnter"    : "ent",
+    "Enter"          : "ent",
+    "NumpadDecimal"  : "dec"
+}
+
+var codeToNum = {
+    "Backquote"    : 0,
+    "Digit1"    : 1,
+    "Digit2"    : 2,
+    "Digit3"    : 3,
+    "Digit4"    : 4,
+    "Digit5"    : 5,
+    "Digit6"    : 6,
+    "Digit7"    : 7,
+    "Digit8"    : 8,
+    "Digit9"    : 9,
+    "Numpad0"   : 0,
+    "Numpad1"   : 1,
+    "Numpad2"   : 2,
+    "Numpad3"   : 3,
+    "Numpad4"   : 4,
+    "Numpad5"   : 5,
+    "Numpad6"   : 6,
+    "Numpad7"   : 7,
+    "Numpad8"   : 8,
+    "Numpad9"   : 9,
+}
+
 function Toggle() {
     isOn = !isOn
     document.getElementById("calc").style.display = isOn ? "flex" : "none"
@@ -12,96 +56,95 @@ function maximize() {
     window.alert("ball")
 }
 
-window.addEventListener("keydown", (e) => { if (e.code == "Backspace")          { OnInput("ce") } })
-window.addEventListener("keydown", (e) => { if (e.code == "Delete")             { OnInput("c") } })
-window.addEventListener("keydown", (e) => { if (e.code == "NumpadDivide")       { OnInput("div") } })
-window.addEventListener("keydown", (e) => { if (e.code == "NumpadMultiply")     { OnInput("mul") } })
-window.addEventListener("keydown", (e) => { if (e.code == "NumpadSubtract")     { OnInput("sub") } })
-window.addEventListener("keydown", (e) => { if (e.code == "NumpadAdd")          { OnInput("add") } })
-window.addEventListener("keydown", (e) => { if (e.code == "NumpadEnter")        { OnInput("ent") } })
-window.addEventListener("keydown", (e) => { if (e.code == "NumpadDecimal")      { OnInput("dec") } })
+window.addEventListener("keydown", (e) => {
+    console.log(e.code)
+    let action = codeToAction[e.code]
+    if (action != null) {
+        OnInput(action)
+        return
+    }
 
-window.addEventListener("keydown", (e) => { if (e.code == "Numpad0")            { OnInput(0) } })
-window.addEventListener("keydown", (e) => { if (e.code == "Numpad1")            { OnInput(1) } })
-window.addEventListener("keydown", (e) => { if (e.code == "Numpad2")            { OnInput(2) } })
-window.addEventListener("keydown", (e) => { if (e.code == "Numpad3")            { OnInput(3) } })
-window.addEventListener("keydown", (e) => { if (e.code == "Numpad4")            { OnInput(4) } })
-window.addEventListener("keydown", (e) => { if (e.code == "Numpad5")            { OnInput(5) } })
-window.addEventListener("keydown", (e) => { if (e.code == "Numpad6")            { OnInput(6) } })
-window.addEventListener("keydown", (e) => { if (e.code == "Numpad7")            { OnInput(7) } })
-window.addEventListener("keydown", (e) => { if (e.code == "Numpad8")            { OnInput(8) } })
-window.addEventListener("keydown", (e) => { if (e.code == "Numpad9")            { OnInput(9) } })
-
+    let numKey = codeToNum[e.code]
+    if (numKey != null) {
+        OnInput(numKey)
+    }
+})
 
 function UpdateOutput() {
-    document.getElementById("outputUpper").innerText = (numCache != 0 ? toString(numCache) + lastAction : "⠀")
-    document.getElementById("outputLower").innerText = num
+    document.getElementById("outputUpper").innerText = (numCache != "0" ? numCache + opToChar[lastAction] : "⠀")
+    document.getElementById("outputLower").innerText = num.substring(0, num.includes(".") ? numLengthLimit + 1 : numLengthLimit)
+}
+
+function Calculate() {
+    if (lastAction != null) {
+        switch (lastAction) {
+            case "div":
+                num = (parseFloat(numCache) / parseFloat(num)).toString()
+                break;
+            case "mul":
+                num = (parseFloat(numCache) * parseFloat(num)).toString()
+                break;
+            case "sub":
+                num = (parseFloat(numCache) - parseFloat(num)).toString()
+                break;
+            case "add":
+                num = (parseFloat(numCache) + parseFloat(num)).toString()
+                break;
+            default:
+                break;
+        }
+    
+        numCache = "0"
+        lastAction = null
+        UpdateOutput()
+
+        return
+    }
 }
 
 function OnInput(button) {
     if (typeof(button) == "string") {
-        switch (key) {
+        switch (button) {
             case "c":
-                num = 0
-                numCache = 0
+                num = "0"
+                numCache = "0"
                 lastAction = null
-                UpdateOutput()
                 break;
             case "ce":
-                num = 0
-                UpdateOutput()
+                num = "0"
                 break;
             case "ent":
-                UpdateOutput()
-                break;
-            default:
-                lastAction = button
-                break;
-        }
-
-        return
-    }
-
-    if (lastAction != null) {
-        switch (lastAction) {
-            case "div":
-                num = num / numCache
-                numCache = 0
-                UpdateOutput()
-                break;
-            case "mul":
-                num = num * numCache
-                numCache = 0
-                UpdateOutput()
-                break;
-            case "sub":
-                num = num - numCache
-                numCache = 0
-                UpdateOutput()
-                break;
-            case "add":
-                num = num + numCache
-                numCache = 0
-                UpdateOutput()
-                break;
-            case "sqrt":
-                num = num ^ numCache
-                numCache = 0
-                UpdateOutput()
+                Calculate()
                 break;
             case "inv":
-                num = -num
-                UpdateOutput()
+                num = (parseFloat(num) * -1).toString()
+                break;
+            case "pow":
+                num = Math.pow(parseFloat(num), 2).toString()
+                break;
+            case "dec":
+                if (num.includes(".")) { break }
+                num = num + "."
                 break;
             default:
+                if (lastAction != null) {
+                    Calculate()
+                }
+
+                lastAction = button
+                numCache = num
+                //num = "0"
                 break;
         }
-        
-        lastAction = null
-        return
+    }
+    else if ((num.includes(".") ? num.length - 1 : num.length) < numLengthLimit) {
+        if (num == "0") {
+            num = button.toString()
+        }
+        else {
+            num += button
+        }
     }
 
-    numStr += button
-    num = Number.parseFloat(numStr)
     UpdateOutput()
 }
